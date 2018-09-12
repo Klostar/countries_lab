708c78c60028b1520ceb32c92b56e75c463f6ad7
Michael Blumenthal <bloomer25@googlemail.com>
2018-09-12 15:04:30 +0100

    adds render function

--- src/views/display_view.js
+++ src/views/display_view.js
@@ -14,7 +14,38 @@ DisplayView.prototype.bindEvents = function () {
 };
 
 DisplayView.prototype.render = function (object) {
-  console.log(object);
+  this.element.innerHTML = '';
+  const countryName = document.createElement('h2');
+  countryName.textContent = object.name;
+  this.element.appendChild(countryName);
+
+  const image = document.createElement('img');
+  image.src = object.flag;
+  image.style = 'width: 50%; box-shadow: 10px 10px grey'
+  this.element.appendChild(image)
+
+  const regionHeader = document.createElement('h2');
+  regionHeader.textContent = 'Region:';
+  this.element.appendChild(regionHeader);
+
+  const region = document.createElement('h3');
+  region.textContent = object.region
+  this.element.appendChild(region);
+
+  const languagesHeader = document.createElement('h2');
+  languagesHeader.textContent = 'Languages:';
+  this.element.appendChild(languagesHeader);
+
+  const list = document.createElement('ul');
+  this.element.appendChild(list);
+  object.languages.forEach((language) => {
+    const entry = document.createElement('li');
+    entry.textContent = `${language.name} (${language.nativeName})`;
+    this.element.appendChild(entry)
+
+  });
+
 };
+
 
 module.exports = DisplayView;
